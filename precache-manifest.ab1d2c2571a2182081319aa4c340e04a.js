fbc518628b849295d07dfee05bc7ef073d978a0c
hyosunko <hyosunko@gmail.com>
2019-02-18 22:32:12 -0800

    Updates

---
+++ precache-manifest.ab1d2c2571a2182081319aa4c340e04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65d5a2aeb2422a5565de",
+    "url": "/video-search/static/js/runtime~main.65d5a2ae.js"
+  },
+  {
+    "revision": "bfd56be55e9ee16a7e19",
+    "url": "/video-search/static/js/main.bfd56be5.chunk.js"
+  },
+  {
+    "revision": "5918dbad8567dc79d88f",
+    "url": "/video-search/static/js/2.5918dbad.chunk.js"
+  },
+  {
+    "revision": "bfd56be55e9ee16a7e19",
+    "url": "/video-search/static/css/main.b962c7c6.chunk.css"
+  },
+  {
+    "revision": "cef47925b7f5ee69c13b543e02332fe3",
+    "url": "/video-search/index.html"
+  }
+];
